f2aeaa5a521b909f2cf322ec1d3287c5b2d26aef
L to the H <113588881+lhmygis@users.noreply.github.com>
2025-03-01 13:34:27 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -16,19 +16,23 @@ def nse_loss(y_true, y_pred):
     numerator = K.sum(K.square(y_pred - y_true), axis=1)
     denominator = K.sum(K.square(y_true - K.mean(y_true, axis=1, keepdims=True)), axis=1)
 
+
+    #Add a small enough number to the denominator to prevent Nan Loss
     return numerator / (denominator+0.01)
 
 
 def nse_metrics(y_true, y_pred):
 
-    y_pred = K.permute_dimensions(y_pred, pattern=(1,0,2))  #[2212,60,1] ->  [60,2218,1]
 
+    y_pred = K.permute_dimensions(y_pred, pattern=(1,0,2))  #[2212,60,1] ->  [60,2218,1]
 
     y_true = y_true[:, :, :]  # Omit values in the spinup period (the first 365 days)
     y_pred = y_pred[:, :, :]  # Omit values in the spinup period (the first 365 days)
 
     numerator = K.sum(K.square(y_pred - y_true), axis=1)
     denominator = K.sum(K.square(y_true - K.mean(y_true, axis=1, keepdims=True)), axis=1)
-    rNSE = numerator / (denominator+0.01)
 
-    return 1.0 - rNSE
+    #Add a small enough number to the denominator to prevent Nan Loss
+    loss =  numerator / (denominator+0.01)
+
+    return 1.0 - loss
